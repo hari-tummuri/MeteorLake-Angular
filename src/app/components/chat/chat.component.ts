f418cf3e3534254c2b69c41464b9468d5ccde5dd
hari-tummuri <104126503+hari-tummuri@users.noreply.github.com>
2024-02-13 11:12:56 +0530

    Update chat.component.ts

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DomSanitizer,SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Agent } from 'src/app/AgentReply';
+import { CasesService } from 'src/app/services/cases.service';
 import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
@@ -18,16 +19,19 @@ export class ChatComponent implements OnInit {
   doc_path : string = "assets/documents/"+this.caseno+".pdf"
   sumDoc : string = "document"
   urlSafe !: SafeResourceUrl;
+  summary !: any;
 
-  constructor(private route: ActivatedRoute, private chatService : ChatService, private sanitizer : DomSanitizer ){}
+  constructor(private route: ActivatedRoute, private chatService : ChatService, private sanitizer : DomSanitizer, private caseService : CasesService ){}
   ngOnInit(){
     // this.path = this.route.snapshot.params['path'];
     localStorage.setItem('page','chat')
+    // localStorage.getItem('caseno')
     console.log(this.doc_path);
     // this.newpath=this.sanitizer.bypassSecurityTrustResourceUrl(this.doc_path);
     // (<HTMLIFrameElement>document.getElementById('doc')).src = this.doc_path;
     // this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.doc_path);
     this.newpath = this.sanitizer.bypassSecurityTrustResourceUrl(this.doc_path);
+    this.getSummaryByCaseno()
   }
 
   messages = [
@@ -40,15 +44,22 @@ export class ChatComponent implements OnInit {
   sendMessage() {
     this.messages.push({ user: "User", text: this.newMessage });
     this.getReply(this.newMessage)
-    this.messages.push({user : "Agent", text: this.agent})
+    // this.messages.push({user : "Agent", text: this.agent})
 
     this.newMessage = "";
   }
 
+  getSummaryByCaseno(){
+    this.summary = this.caseService.getSummaryStatic()
+    console.log(this.summary);
+    
+  }
   getReply(question : any){
     this.chatService.getReply(question).subscribe({
       next: (response: any) => {
-        this.agent = response['Answer']
+        this.agent = response['response']
+        console.log('Response:', this.agent);
+      this.messages.push({ user: "Agent", text: this.agent });  
       },
       error: (error: any) => {
         console.log();
