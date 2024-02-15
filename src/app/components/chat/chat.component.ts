3af4c14b224895bb96012b9488fc2a94a85a3d97
hari-tummuri <104126503+hari-tummuri@users.noreply.github.com>
2024-02-15 11:33:05 +0530

    Update chat.component.ts

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,14 +1,18 @@
+// import { animation } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer,SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Agent } from 'src/app/AgentReply';
 import { CasesService } from 'src/app/services/cases.service';
 import { ChatService } from 'src/app/services/chat.service';
+// import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
+  // animations:[trigger('isLoading',[state('start',style({opacity : 1, transform : 'scale(1.0)'})),state('end',style({opacity : 0, transform : 'scale(0.0)'})), transition('start <=> end', animate('500ms ease-in-out'))])]
+
 })
 export class ChatComponent implements OnInit {
 
@@ -20,6 +24,10 @@ export class ChatComponent implements OnInit {
   sumDoc : string = "document"
   urlSafe !: SafeResourceUrl;
   summary !: any;
+  loading : boolean = false
+
+
+
 
   constructor(private route: ActivatedRoute, private chatService : ChatService, private sanitizer : DomSanitizer, private caseService : CasesService ){}
   ngOnInit(){
@@ -35,7 +43,7 @@ export class ChatComponent implements OnInit {
   }
 
   messages = [
-    { user: "Law Agent", text: "Hi, You can ask your queries related to uploaded document" },
+    { user: "Agent", text: "Hi, You can ask your queries related to uploaded document" },
     // { user: "User", text: "Hello" },
     // Add more messages for initial chat history
   ];
@@ -43,6 +51,7 @@ export class ChatComponent implements OnInit {
 
   sendMessage() {
     this.messages.push({ user: "User", text: this.newMessage });
+    this.loading = true
     this.getReply(this.newMessage)
     // this.messages.push({user : "Agent", text: this.agent})
 
@@ -52,14 +61,15 @@ export class ChatComponent implements OnInit {
   getSummaryByCaseno(){
     this.summary = this.caseService.getSummaryStatic()
     console.log(this.summary);
-    
+
   }
   getReply(question : any){
     this.chatService.getReply(question).subscribe({
       next: (response: any) => {
+        this.loading = false
         this.agent = response['response']
         console.log('Response:', this.agent);
-      this.messages.push({ user: "Agent", text: this.agent });  
+      this.messages.push({ user: "Agent", text: this.agent });
       },
       error: (error: any) => {
         console.log();
