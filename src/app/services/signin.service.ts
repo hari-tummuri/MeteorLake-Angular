7b4e30ed5772006e81e67b5d9ff77d31d7cfea6f
hari-tummuri <104126503+hari-tummuri@users.noreply.github.com>
2024-02-16 11:48:38 +0530

    Update signin.service.ts

--- src/app/services/signin.service.ts
+++ src/app/services/signin.service.ts
@@ -6,7 +6,7 @@ import { User } from '../User';
 })
 export class SigninService {
 
-  users : User[] = [{"email" : "naveen@gmail.com", "password": "Demo123", "profileName":"Naveen", "profileimage": "https://www.asterhospitals.in/sites/default/files/2023-05/Dr%20Naveen%20Chandra%20for%20Web.jpg", "won": 50, "loss": 10, "inProgress":7 },
+  users : User[] = [{"email" : "naveen@gmail.com", "password": "Demo123", "profileName":"Naveen", "profileimage": "https://media.licdn.com/dms/image/C4E03AQErNeIsqNvsmA/profile-displayphoto-shrink_800_800/0/1626780671355?e=2147483647&v=beta&t=kDF_HBtmb1RKMgnwak2-p8HNiU2dBe1wd2di_Y3-QFE", "won": 50, "loss": 10, "inProgress":7 },
   {"email" : "abhijeet.kadam@wipro.com", "password": "Abhijeet123", "profileName":"Abhijeet", "profileimage": "https://media.istockphoto.com/id/503040171/photo/middle-eastern-businessman-portrait.jpg?s=612x612&w=0&k=20&c=7t6c_HQHfUZNgrVtR-G1rQpJAMaCbFsuxppDRKBnXDw=", "won": 55, "loss": 11, "inProgress":15 }
 ]
   constructor() { }
